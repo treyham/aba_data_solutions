7d5e15dae56d00f2465bb10c4d89ac2d7b9ce608
TreyHammy <treyhammy13@gmail.com>
2021-08-06 19:37:02 -0400

    TODO: fix every other save breaks

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -22,8 +22,12 @@ server.listen(3000, (err) => {
     console.error(err)
     process.exit(1)
   }
-  console.log(`\
+  console.log(`
+  Testing 1 2 3 4
   🚀 Server ready at: http://localhost:3000/graphiql
   ⭐️ See sample queries: http://pris.ly/e/ts/graphql-fastify-sdl-first#using-the-graphql-api
   `)
 })
+
+// watch ben's vid on how to do the ts compile stuff
+// right now if it breaks when you save just save again
