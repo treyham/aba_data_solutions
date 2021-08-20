6b64f1832c7bd1ce7ce22c9fc1fe4c12ebe66d46
TreyHammy <treyhammy13@gmail.com>
2021-08-20 14:36:51 -0400

    need to fix: typedefs & resolvers

--- packages/server/src/shema.ts
+++ packages/server/src/shema.ts
@@ -1,6 +1,7 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
-import { DateTimeResolver } from 'graphql-scalars'
+// import { DateTimeResolver } from 'graphql-scalars'
 import { Context } from './context'
+import { resolvers } from '../../api/type-graphql/generated/index'
 
 const fs = require('fs');
 const path = require('path');
@@ -16,9 +17,10 @@ const typeDefs = fs.readFileSync(
 
 
 export const schema = makeExecutableSchema({
+  typeDefs,
   resolvers: {
     Query,
     Mutation
   },
-  typeDefs
+  
 })
