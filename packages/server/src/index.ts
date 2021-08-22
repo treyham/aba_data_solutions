19d944a84c4a98c04aaf52bd91e76a88755fb3eb
TreyHammy <treyhammy13@gmail.com>
2021-08-22 13:02:55 -0400

    changed server to index

---
+++ packages/server/src/index.ts
@@ -0,0 +1,37 @@
+import fastify from 'fastify'
+import mercurius, {
+  IFieldResolver,
+  IResolvers,
+  MercuriusContext,
+} from 'mercurius'
+import { schema } from './shema.js'
+import { context } from './context.js'
+
+declare module 'mercurius' { }
+
+async function main() {
+  const server = fastify()
+  const dbPort = 3000
+  const __prod__ = false
+  console.log(`Dev: ${!__prod__}`)
+  server.register(mercurius, {
+    schema,
+    graphiql: !__prod__,
+    context: () => context
+  })
+  
+  server.listen(dbPort, (err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(`
+    🚀 Server ready at: http://localhost:${dbPort}/graphiql
+    ⭐️ See sample queries: http://pris.ly/e/ts/graphql-fastify-sdl-first#using-the-graphql-api
+    `)
+  })
+}
+
+
+// watch ben's vid on how to do the ts compile stuff
+// right now if it breaks when you save just save again
