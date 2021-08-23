428cfa167ba1c490e1cfb4cf83e3d99f59b29cbe
TreyHammy <treyhammy13@gmail.com>
2021-08-23 17:57:59 -0400

    temp encryption

---
+++ packages/server/src/api/module/employee/create.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata"
+import * as bcrypt from 'bcryptjs'
+import { prisma } from ".prisma/client"
+import { PrismaClient } from '@prisma/client'
+import {
+    Resolver,
+    Query,
+    Mutation,
+    Arg,
+    // buildSchema,
+    // FieldResolver,
+    // Ctx,
+    // Root,
+  } from "type-graphql"
+  
+
+@Resolver()
+export class CreateEmployeeResolver {
+  @Query(() => String)
+  async hello() {
+    return "world"
+  }
+
+  @Mutation(() => String)
+  async createEmployee(
+    @Arg('fullname') fullName: string,
+    @Arg('displayName') displayName: string,
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Arg('birthdate') birthdate: string,
+    @Arg('tempPosition') tempPosition: string
+    //   @Arg('created_at') created_at: string,
+    //   @Arg('updated_at') updated_at: string,
+  ) {
+    const hashedPass = await bcrypt.hash(password, 'temp')
+    // const newEmployee = await prisma.
+    return fullName
+  }
+}
