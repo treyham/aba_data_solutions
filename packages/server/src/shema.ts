356b23fefdef5e620bcd4e03c74b203df2818178
TreyHammy <treyhammy13@gmail.com>
2021-08-24 14:58:13 -0400

    organize imports

--- packages/server/src/shema.ts
+++ packages/server/src/shema.ts
@@ -1,11 +1,10 @@
 import "reflect-metadata"
 import { buildSchema } from "type-graphql"
-import { Context } from './context'
 import { CreateEmployeeResolver } from './api/module/employee/create'
+// import { resolvers } from "@generated/type-graphql"
 
 export const schema = await buildSchema({
   resolvers: [
-    // ...resolvers,
     CreateEmployeeResolver,
   ]
 })
