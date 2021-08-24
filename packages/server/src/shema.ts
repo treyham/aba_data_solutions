f2bc722e8a136d5b4598ce109303446c679e9cd7
TreyHammy <treyhammy13@gmail.com>
2021-08-24 17:17:57 -0400

    import and register generated resolvers

--- packages/server/src/shema.ts
+++ packages/server/src/shema.ts
@@ -1,10 +1,91 @@
 import "reflect-metadata"
 import { buildSchema } from "type-graphql"
+
+// generated
+import * as models from '@generated/type-graphql/models'
+import {
+  AdministrationCrudResolver,
+  BcbaCrudResolver,
+  BillingCrudResolver,
+  BillingObjectCrudResolver,
+  ClientCrudResolver,
+  EmployeeCrudResolver,
+  LoginCrudResolver,
+  PersonalInfoCrudResolver,
+  PlanCrudResolver,
+  ProgramCrudResolver,
+  ProviderCrudResolver,
+  RbtCrudResolver,
+  RecordCrudResolver,
+  SessionCrudResolver,
+  TreatmentPlanCrudResolver,
+  TreatmentPlan_to_ProgramCrudResolver
+} from '@generated/type-graphql/resolvers/crud'
+import {
+  AdministrationRelationsResolver,
+  BcbaRelationsResolver,
+  BillingRelationsResolver,
+  // BillingObjectRelationsResolver,
+  ClientRelationsResolver,
+  EmployeeRelationsResolver,
+  LoginRelationsResolver,
+  // PersonalInfoRelationsResolver,
+  PlanRelationsResolver,
+  ProgramRelationsResolver,
+  ProviderRelationsResolver,
+  RbtRelationsResolver,
+  RecordRelationsResolver,
+  SessionRelationsResolver,
+  TreatmentPlanRelationsResolver,
+  // TreatmentPlan_to_PrograRelationsdResolver
+} from '@generated/type-graphql/resolvers/relations'
+
+// custom
 import { CreateEmployeeResolver } from './api/module/employee/create'
-// import { resolvers } from "@generated/type-graphql"
+
 
 export const schema = await buildSchema({
   resolvers: [
-    CreateEmployeeResolver,
+    /**
+     *                        G e n e r a t e d 
+     */ 
+    // types  
+    // crud 
+    AdministrationCrudResolver,
+    BcbaCrudResolver,
+    BillingCrudResolver,
+    BillingObjectCrudResolver,
+    ClientCrudResolver,
+    EmployeeCrudResolver,
+    LoginCrudResolver,
+    PersonalInfoCrudResolver,
+    PlanCrudResolver,
+    ProgramCrudResolver,
+    ProviderCrudResolver,
+    RbtCrudResolver,
+    RecordCrudResolver,
+    SessionCrudResolver,
+    TreatmentPlanCrudResolver,
+    TreatmentPlan_to_ProgramCrudResolver,
+    // relations 
+    AdministrationRelationsResolver,
+    BcbaRelationsResolver,
+    BillingRelationsResolver,
+    ClientRelationsResolver,
+    EmployeeRelationsResolver,
+    LoginRelationsResolver,
+    PlanRelationsResolver,
+    ProgramRelationsResolver,
+    ProviderRelationsResolver,
+    RbtRelationsResolver,
+    RecordRelationsResolver,
+    SessionRelationsResolver,
+    TreatmentPlanRelationsResolver,
+    // TreatmentPlan_to_PrograRelationsdResolver
+    /**
+     *                          C u s t o m 
+     */ 
+    // custom
+    CreateEmployeeResolver
   ]
 })
