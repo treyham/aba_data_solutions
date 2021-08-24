d8b55137c7cb449226cc6b9e24f7dcd3f24544c4
TreyHammy <treyhammy13@gmail.com>
2021-08-23 22:00:50 -0400

    added context, which is prisma instance

--- packages/server/src/api/module/employee/create.ts
+++ packages/server/src/api/module/employee/create.ts
@@ -1,6 +1,6 @@
+import { Context } from './../../../../types/src/context.d';
 import "reflect-metadata"
 import * as bcrypt from 'bcryptjs'
-import { prisma } from ".prisma/client"
 import { PrismaClient } from '@prisma/client'
 import {
     Resolver,
@@ -9,7 +9,7 @@ import {
     Arg,
     // buildSchema,
     // FieldResolver,
-    // Ctx,
+    Ctx,
     // Root,
   } from "type-graphql"
   
@@ -21,19 +21,22 @@ export class CreateEmployeeResolver {
     return "world"
   }
 
-//   @Mutation(() => String)
-//   async createEmployee(
-//     @Arg('fullname') fullName: string,
-//     @Arg('displayName') displayName: string,
-//     @Arg('email') email: string,
-//     @Arg('password') password: string,
-//     @Arg('birthdate') birthdate: string,
-//     @Arg('tempPosition') tempPosition: string
-//     //   @Arg('created_at') created_at: string,
-//     //   @Arg('updated_at') updated_at: string,
-//   ) {
-//     const hashedPass = await bcrypt.hash(password, 'temp')
-//     // const newEmployee = await prisma.
-//     return fullName
-//   }
+  
+
+  @Mutation(() => String)
+  async createEmployee(
+    @Ctx() ctx: Context,
+    @Arg('fullname') fullName: string,
+    @Arg('displayName') displayName: string,
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Arg('birthdate') birthdate: string,
+    @Arg('tempPosition') tempPosition: string
+    //   @Arg('created_at') created_at: string,
+    //   @Arg('updated_at') updated_at: string,
+  ) {
+    const hashedPass = await bcrypt.hash(password, 'temp')
+    //const newEmployee = await ctx.prisma.employee.create()
+    return fullName
+  }
 }
