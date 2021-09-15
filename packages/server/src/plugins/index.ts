31ba1b469c39c1c37f634315b11739fa61589a18
TreyHammy <treyhammy13@gmail.com>
2021-09-15 17:39:46 -0400

    init

--- packages/server/src/plugins/index.ts
+++ packages/server/src/plugins/index.ts
@@ -1,1 +1,18 @@
-export * as db from './db'
+import fp from 'fastify-plugin'
+import auth, { AuthPluginOptions } from './auth'
+import { dbPlugin, DbPluginOptions } from './db'
+
+// TODO AppPluginOptions interface
+export interface AppPluginOptions {
+  // Specify Support plugin options here
+  authOpts: AuthPluginOptions
+  dbOpts: DbPluginOptions
+}
+
+// The use of fastify-plugin is required to be able to export the decorators to the outer scope
+export default fp<AppPluginOptions>(async (fastify, opts) => {
+  fastify.decorate('plugins', async () => {
+    fastify.register(auth, opts.authOpts)
+    fastify.register(dbPlugin, opts.dbOpts)
+  })
+})
