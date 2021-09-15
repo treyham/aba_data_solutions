1aa310e1536b1ada923a558863f955ec2af3cec0
TreyHammy <treyhammy13@gmail.com>
2021-09-15 17:39:00 -0400

    session & cookies

--- packages/server/src/plugins/auth/index.ts
+++ packages/server/src/plugins/auth/index.ts
@@ -1,13 +1,36 @@
 import fp from 'fastify-plugin'
-// TODO auth plugin
-export interface AuthPluginOptions {
-  // Specify Support plugin options here
+import cookie, { FastifyCookieOptions } from 'fastify-cookie'
+import fastifySession from 'fastify-session'
+import { config } from '@app/config'
+import FastifySessionPlugin from 'fastify-session'
+import { nextTick } from 'process'
+
+export interface FastifySessionOptions {
+  secret: string
+  saveUninitialized: boolean
+  cookie: FastifyCookieOptions
 }
 
-// The use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
-export default fp<AuthPluginOptions>(async (fastify, opts) => {
-  fastify.decorate('database', function () {
-    return 'hugs'
+// TODO AuthPluginOptions interface
+export interface AuthPluginOptions {
+  // Specify Support plugin options here
+  session: FastifySessionOptions
+  cookie: FastifyCookieOptions
+}
+
+export interface AuthPlugin {
+  opts: AuthPluginOptions
+}
+
+// The use of fastify-plugin is required to be able to export the decorators to the outer scope
+export default fp<AuthPluginOptions>(async (fastify, opts: AuthPluginOptions) => {
+  fastify.decorate('auth', async () => {
+    await fastify.register(cookie)
+    await fastify.register(fastifySession, {
+      secret: opts.session.secret.split(','),              // allows comma delim string of secrets
+      saveUninitialized: config.env.isProd,
+      cookie: opts.cookie
+    } as any)                                                   // TODO fix any
   })
 })
+
