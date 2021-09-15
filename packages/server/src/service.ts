7c5bd35d8a1ea8d52670cd8c3086c81973a3c8c4
TreyHammy <treyhammy13@gmail.com>
2021-09-15 16:03:01 -0400

    point to proper directories

--- packages/server/src/service.ts
+++ packages/server/src/service.ts
@@ -1,59 +1,38 @@
-import AutoLoad, {AutoloadPluginOptions} from 'fastify-autoload';
-import { FastifyPluginAsync } from 'fastify';
+import { db } from './plugins'
+import AutoLoad, {AutoloadPluginOptions} from 'fastify-autoload'
+import { FastifyPluginAsync } from 'fastify'
 import { config } from '@app/config'
 
 console.log('appService')
-
 export type AppOptions = {
   // Place your custom options for app below here.
+  dbOpts?: db.DbPluginOptions
+
 } & Partial<AutoloadPluginOptions>;
 
-const app: FastifyPluginAsync<AppOptions> = async (
+const plugins: FastifyPluginAsync<AppOptions> = async (
     fastify,
     opts
 ): Promise<void> => {
   // Place here your custom code!
-  var hoyeah = 'hhhhhhhhhhhhHHHHHHHHHHHHHHHHHHHHHHHHHHOOOOOOOOOOOOOOOOO0000000000ooo0000OOOOOOOYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEaaaaaaaaawwwWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWhhhhhhhhhhhhhhhhhHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'
-
-  config.isProd? 
-    console.log('uhhhh, Improvise!!'): 
-    async () => {
-      console.log('ok, develop time')
-      await new Promise(r => setTimeout(r, 1400))
-      console.log('uhhm')
-      await new Promise(r => setTimeout(r, 666))
-      console.log('uuuuhhhurrrhhhh')
-      await new Promise(r => setTimeout(r, 2000))
-      console.log('can i get a \'ho yeah\'')
-      await new Promise(r => setTimeout(r, 300))
-      console.log('yeah')
-      await new Promise(r => setTimeout(r, 50))
-      console.log('go')
-      async () => {
-        [...hoyeah].forEach(async char => {
-          await new Promise(r => setTimeout(r, Math.random() * (50 - 10) + 10))
-          console.log(char)
-        })
-      }
-    }
+  config.constant.intro(config.env.isProd)
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
   void fastify.register(AutoLoad, {
-    dir: config.paths.server.plugins,
+    dir: config.constant.path.server.plugins,
     options: opts
   })
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   void fastify.register(AutoLoad, {
-    dir: config.paths.server.routes,
+    dir: config.constant.path.server.routes,
     options: opts
   })
+}
 
-};
-
-export default app;
-export { app }
+export default plugins;
+export { plugins }
