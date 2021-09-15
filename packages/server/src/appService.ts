e0ce9d1f30e099a13e23b17b40630212835faf38
TreyHammy <treyhammy13@gmail.com>
2021-09-15 03:18:02 -0400

    intro

--- packages/server/src/appService.ts
+++ packages/server/src/appService.ts
@@ -13,7 +13,29 @@ const appService: FastifyPluginAsync<AppOptions> = async (
     opts
 ): Promise<void> => {
   // Place here your custom code!
+  var hoyeah = 'hhhhhhhhhhhhHHHHHHHHHHHHHHHHHHHHHHHHHHOOOOOOOOOOOOOOOOO0000000000ooo0000OOOOOOOYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEaaaaaaaaawwwWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWhhhhhhhhhhhhhhhhhHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'
 
+  config.isProd? 
+    console.log('uhhhh, Improvise!!'): 
+    async () => {
+      console.log('ok, develop time')
+      await new Promise(r => setTimeout(r, 1400))
+      console.log('uhhm')
+      await new Promise(r => setTimeout(r, 666))
+      console.log('uuuuhhhurrrhhhh')
+      await new Promise(r => setTimeout(r, 2000))
+      console.log('can i get a \'ho yeah\'')
+      await new Promise(r => setTimeout(r, 300))
+      console.log('yeah')
+      await new Promise(r => setTimeout(r, 50))
+      console.log('go')
+      async () => {
+        [...hoyeah].forEach(async char => {
+          await new Promise(r => setTimeout(r, Math.random() * (50 - 10) + 10))
+          console.log(char)
+        })
+      }
+    }
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
