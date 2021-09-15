24e35370c07fcf72d2ca802dc469549f94f6fc79
TreyHammy <treyhammy13@gmail.com>
2021-09-15 16:02:41 -0400

    autoload server plugins

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,66 +1,39 @@
-import { context, schema } from '@app/api'
+import { context } from '@app/api'
 import { config } from '@app/config' // must be after import from @app/api
-import AltairFastify from 'altair-fastify-plugin'
 import fastify from 'fastify'
-import cookie, { FastifyCookieOptions } from 'fastify-cookie'
-import fastifySession from 'fastify-session'
-import mercurius, {
-  IFieldResolver,
-  IResolvers,
-  MercuriusContext,
-} from 'mercurius'
-import { appService } from './appService'
-
+import { plugins } from './service'
+// import closeWithGrace from 'close-with-grace'
 declare module 'mercurius' {}
 
 async function main() {
-  const server = fastify({ logger: !config.isProd})
-  
+  const server = fastify({ logger: !config.env.isProd})
   // register plugins
-  await server.register(appService)
-  // await server.register(mercurius, {
-  //   schema,
-  //   graphiql: false,
-  //   ide: false,
-  //   path: '/graphql',
-  //   context: () => (context)                              // provide the prisma instance to the context
-  // })
-  // // See sample queries: http://pris.ly/e/ts/graphql-fastify-sdl-first#using-the-graphql-api  
-  // await server.register(AltairFastify, {
-  //   path: '/altair',
-  //   baseURL: '/altair/',
-  //   endpointURL: '/graphql'                               // should be the same as the mercurius 'path'
-  // })
-  // await server.register(cookie)
-  // await server.register(fastifySession, {
-  //   secret: config.sessionSecret.split(','),              // allows comma delim string of secrets
-  //   saveUninitialized: false,
-  //   cookie: {
-  //     httpOnly: true,
-  //     secure: config.isProd                               // options for parsing cookies
-  //   } as FastifyCookieOptions
-  // } as any)                                               // TODO fix any                      
-  
+  await server.register(plugins)
+  // old stuff moved to
+  // packages/server/src/plugins/db/schema/index.ts
+  // packages/server/src/plugins/auth/index.ts   
   // add hooks
   server.addHook('preHandler', (request, reply, next) => {
     console.log("HOOK", "sessionId: ", request.session.sessionId, "\nencryptedSessionId: ", request.session.encryptedSessionId)
   })
-
-  //if (config.node_dev) fastify.log.info()
+  // const closeListeners = closeWithGrace({ delay: 500 }, async ({ err:  }) => {
+  //   if (err) {
+  //     server.log.error(err)
+  //   }
+  //   await server.close()
+  // })
+  // server.addHook('onClose', async (instance, done) => {
+  //   closeListeners.uninstall()
+  //   done()
+  // })
   return server
-
 }
-/**
- * If the below script runs multiple times in the context of a long-
- * running application without calling ```$disconnect()```, a new
- * connection pool is created with each new instance of
- * ```{ PrismaClient }```.
- */
+
 main()
   // https://github.com/fastify/middie
-  .then(server => server.listen(config.serverPort as string, () => {
+  .then(server => server.listen(config.env.serverPort as string, () => {
     console.log(`
-    🚀 Dev Server ready at: http://localhost:${config.serverPort}/altair
+    🚀 Dev Server ready at: http://localhost:${config.env.serverPort}/altair
     ⭐️ You rock!
     `)
   }))                                                       
@@ -70,5 +43,10 @@ main()
     console.log("Disconnecting...") 
   })
 
-
+/**
+ * If the above script runs multiple times in the context of a long-
+ * running application without calling ```$disconnect()```, a new
+ * connection pool is created with each new instance of
+ * ```{ PrismaClient }```.
+ */
 // watch ben's vid on how to do the ts compile stuff
