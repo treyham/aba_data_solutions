410cb71b2ef1fdc2fd03623eab6cd85134de4be4
TreyHammy <treyhammy13@gmail.com>
2021-09-15 13:58:17 -0400

    fixed db plugin

--- packages/server/src/plugins/db/index.ts
+++ packages/server/src/plugins/db/index.ts
@@ -1,13 +1,13 @@
-import fp from 'fastify-plugin'
-// TODO db plugin
-export interface DbPluginOptions {
-  // Specify Support plugin options here
-}
+// import fp from 'fastify-plugin'
+// // TODO db plugin
+// export interface DbPluginOptions {
+//   // Specify Support plugin options here
+// }
 
-// The use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
-export default fp<DbPluginOptions>(async (fastify, opts) => {
-  fastify.decorate('database', function () {
-    return 'hugs'
-  })
-})
+// // The use of fastify-plugin is required to be able
+// // to export the decorators to the outer scope
+// export default fp<DbPluginOptions>(async (fastify, opts) => {
+//   fastify.decorate('database', function () {
+//     return 'hugs'
+//   })
+// })
--- packages/server/src/plugins/db/schema/index.ts
+++ packages/server/src/plugins/db/schema/index.ts
@@ -1,12 +1,12 @@
 import fp from 'fastify-plugin'
 // TODO schema plugin
-export interface SchemaPluginOptions {
+export interface DbSchemaPluginOptions {
   // Specify Support plugin options here
 }
 
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
-export default fp<SchemaPluginOptions>(async (fastify, opts) => {
+export default fp<DbSchemaPluginOptions>(async (fastify, opts) => {
   fastify.decorate('database', function () {
     return 'hugs'
   })
