bad60bd1d98d8c7200d9c5c04a711f970706d7ea
TreyHammy <treyhammy13@gmail.com>
2021-09-15 17:38:37 -0400

    isolated db plugins

--- packages/server/src/plugins/db/index.ts
+++ packages/server/src/plugins/db/index.ts
@@ -1,13 +1,14 @@
-// import fp from 'fastify-plugin'
-// // TODO db plugin
-// export interface DbPluginOptions {
-//   // Specify Support plugin options here
-// }
+import fp from 'fastify-plugin'
+import { DbSchemaPluginOptions } from './schema'
 
-// // The use of fastify-plugin is required to be able
-// // to export the decorators to the outer scope
-// export default fp<DbPluginOptions>(async (fastify, opts) => {
-//   fastify.decorate('database', function () {
-//     return 'hugs'
-//   })
-// })
+export interface DbPluginOptions {
+  // Specify Support plugin options here
+  schemaOpts?: DbSchemaPluginOptions
+}
+
+// The use of fastify-plugin is required to be able to export the decorators to the outer scope
+export const dbPlugin = fp<DbPluginOptions>(async (fastify, opts) => {
+  fastify.decorate('db', function () {
+    fastify.register(require('./schema'))
+  })
+})
--- packages/server/src/plugins/db/schema/index.ts
+++ packages/server/src/plugins/db/schema/index.ts
@@ -1,13 +1,31 @@
+import { context, schema } from '@app/api'
+import AltairFastify from 'altair-fastify-plugin'
 import fp from 'fastify-plugin'
-// TODO schema plugin
+import mercurius from 'mercurius'
+// import mercurius, {
+//   IFieldResolver,
+//   IResolvers,
+//   MercuriusContext
+// } from 'mercurius'
+// TODO DbSchemaPluginOptions interface
 export interface DbSchemaPluginOptions {
   // Specify Support plugin options here
+
 }
 
-// The use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
+// The use of fastify-plugin is required to be able to export the decorators to the outer scope
 export default fp<DbSchemaPluginOptions>(async (fastify, opts) => {
-  fastify.decorate('database', function () {
-    return 'hugs'
+  await fastify.register(mercurius, {
+    schema,
+    graphiql: false,
+    ide: false,
+    path: '/graphql',
+    context: () => (context)                              // provide the prisma instance to the context
+  })
+  // See sample queries: http://pris.ly/e/ts/graphql-fastify-sdl-first#using-the-graphql-api  
+  await fastify.register(AltairFastify, {
+    path: '/altair',
+    baseURL: '/altair/',
+    endpointURL: '/graphql'                               // should be the same as the mercurius 'path'
   })
 })
