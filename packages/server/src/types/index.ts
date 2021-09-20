e1b09c3e0996f6512ca8eaafefa4b7e2c540ab51
TreyHammy <treyhammy13@gmail.com>
2021-09-20 01:21:38 -0400

    added opts types

--- packages/server/src/types/index.ts
+++ packages/server/src/types/index.ts
@@ -1,26 +1,75 @@
-import fp from 'fastify-plugin'
-import { AuthPluginOpts, DbPluginOpts } from '../plugins'
+import { Context } from '@app/api'
+import { FastifyInstance } from 'fastify'
+import { FastifyCookieOptions } from 'fastify-cookie'
+import { MercuriusSchemaOptions } from 'mercurius'
+
 
 // using declaration merging, add your plugin props to the appropriate fastify interfaces
 declare module 'fastify' {
   interface FastifyRequest {
-    servicePluginProp: ServiceOpts
+    PluginProp: PluginOpts
+    AuthProp: AuthOpts
+    DbProp: DbOpts
   }
   interface FastifyReply {
-    servicePluginProp: ServiceOpts
+    PluginProp: PluginOpts
+    AuthProp: AuthOpts
+    DbProp: DbOpts
   }
 }
 
-export interface ServiceOpts {
-  // Specify Support plugin options here
-  authOpts: AuthPluginOpts
-  dbOpts: DbPluginOpts
+
+// auth
+export interface FastifySessionOpts {
+  secret: string
+  saveUninitialized: boolean
+  cookie: FastifyCookieOptions
 }
+
+// TODO AuthPluginOptions interface
+export interface AuthOpts {
+  // Specify Support plugin options here
+  session: FastifySessionOpts
+  cookie: FastifyCookieOptions
+}
+
+// database
+export interface MercuriusPluginOpts {
+  schema: MercuriusSchemaOptions
+  graphiql: boolean
+  ide: boolean
+  path: string
+  context: Context
+}
+export interface AltairPluginOpts {
+  path: string
+  baseURL: string
+  endpointURL: string
+}
+export interface DbOpts {
+  // Specify Support plugin options here
+  MercuriusPluginOpts: MercuriusPluginOpts
+  AltairaPluginOpts: AltairPluginOpts
+}
+
+// validation
+
+
+
+// server
+export interface PluginOpts {
+  // Specify Support plugin options here
+  authOpts: AuthOpts
+  dbOpts: DbOpts
+}
+
 
 
 // export plugin using fastify-plugin
-export default fp(async (fastify, options): Promise<void> => { 
-  fastify.decorate('service', options) 
-}, 
-'3.x') 
+// export default fp(async (fastify: FastifyInstance, options: pluginOpts): Promise<void> => { 
+//   fastify.decorate('plugins', options)
+//   fastify.decorate('auth', options.AuthPluginOpts)
+//   fastify.decorate('db', options.DbPluginOpts)
+// }, 
+// '3.x') 
 
