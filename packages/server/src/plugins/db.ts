b1ca91228b3e68bfea489fe4d1d2019c2bc72c9d
TreyHammy <treyhammy13@gmail.com>
2021-09-20 01:21:10 -0400

    register mercurius plugin with graphql schema

---
+++ packages/server/src/plugins/db.ts
@@ -0,0 +1,29 @@
+import { context, schema } from '@app/api'
+import { FastifyInstance } from 'fastify'
+import fp from 'fastify-plugin'
+import mercurius , {
+  IFieldResolver,
+  IResolvers,
+  MercuriusContext,
+} from 'mercurius'
+import { DbOpts } from '../types'
+declare module 'mercurius' {}
+
+
+// The use of fastify-plugin is required to be able to export the decorators to the outer scope
+export default fp(async (fastify: FastifyInstance, opts: DbOpts) => {
+  return await fastify
+    .register(mercurius, {
+      schema,
+      graphiql: false,
+      ide: false,
+      path: '/api',
+      context: () => (context)
+    })
+})
+// When using .decorate you have to specify added properties for Typescript
+// declare module 'fastify' {
+//   export interface FastifyInstance {
+//     someSupport(): string;
+//   }
+// }
